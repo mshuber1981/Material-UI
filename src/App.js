13cc3ecf99da348c69bdcf99b7b706cee36b592f
Michael Huber <michael.huber1981@outlook.com>
2022-03-25 09:36:23 -0500

    First commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <a
+          className="App-link"
+          href="https://www.udemy.com/share/102yXc3@DnR_cJNygYC9o0Y4DFvried6gCErpUkT_4QCBJlwcsBs-jmO-a6ML9SQrSLEcWvo/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Material UI
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
