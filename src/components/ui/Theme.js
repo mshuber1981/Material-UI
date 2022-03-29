9f4512ce4ddf37a8e2d645a4571004948fdd5461
Michael Huber <michael.huber1981@outlook.com>
2022-03-29 12:49:37 -0500

    Creating basic theme

---
+++ src/components/ui/Theme.js
@@ -0,0 +1,22 @@
+// MUI - https://v4.mui.com/customization/theming/#createtheme-options-args-theme
+import { createTheme } from "@material-ui/core";
+
+// Main Colors
+const arcBlue = "#0B72B9";
+const arcOrange = "#FFBA60";
+
+// https://v4.mui.com/customization/default-theme/#default-theme
+export default createTheme({
+  palette: {
+    common: {
+      blue: `${arcBlue}`,
+      orange: `${arcOrange}`,
+    },
+    primary: {
+      main: `${arcBlue}`,
+    },
+    secondary: {
+      main: `${arcOrange}`,
+    },
+  },
+});
