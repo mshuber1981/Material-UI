2fef71bc4238d595c6524574abc4d8e9a2de76f1
Michael Huber <michael.huber1981@outlook.com>
2022-05-30 08:59:27 -0500

    Testing first animation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,21 @@
 import React from "react";
+import Lottie from "react-lottie";
+import animationData from "../animations/landinganimation/data";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({}));
 
 export default function Home() {
-  return (
-    <>
-      <div style={{ minHeight: "75vh" }}>
-        <h2>Home</h2>
-      </div>
-    </>
-  );
+  const classes = useStyles();
+
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
+  return <Lottie options={defaultOptions} height={"100%"} width={"100%"} />;
 }
